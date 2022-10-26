37446c8b03d199e8bac64aef45e8fb005e043a5c
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-10-25 21:05:32 -0400

    connect to database

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
